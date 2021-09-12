a110656f58c26b6a6d288e6b9d18def0ecee3fd9
bappyasif <bappyasif@hotmail.com>
2021-09-13 03:09:24 +0600

    fixed textarea auto adjusting height when exceeds initial limit, along with addiotional tweet media inclusions, currently working on file uploader and gif view and upload

--- src/components/home-page/navigation-panels/left-side/tweet-modal/index.js
+++ src/components/home-page/navigation-panels/left-side/tweet-modal/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { deleteIcon, everybodyIcon, mentionedIcon, peopleIcon } from '../svg-resources';
+import { copyIcon, deleteIcon, everybodyIcon, gifIcon, imageIcon, mentionedIcon, peopleIcon, pollIcon, scheduleIcon } from '../svg-resources';
 import '../../styles/tweet-modal.css'
+import {GiphyFetch} from '@giphy/js-fetch-api';
+import {Grid} from '@giphy/react-components';
+const giphyFetch = new GiphyFetch("sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh");
 
 function TweetModal() {
     let [toggleModality, setToggleModality] = useState(false);
@@ -56,7 +59,22 @@ function TweetModal() {
         
     }, [])
 
-    let handleTweetTextChanges = evt => setTweetText(evt.target.value)
+    // let handleTweetTextChanges = evt => setTweetText(evt.target.value)
+    let handleTweetTextChanges = evt => {
+        adjustHeight(evt);
+        setTweetText(evt.target.value)
+    }
+
+    let adjustHeight = (evt) => {
+        let element = evt.target;
+        element.style.height = "81.6px";
+        element.style.height = (1+element.scrollHeight)+"px";
+        // let el = evt.target;
+        // el.style.height = (el.scrollHeight > el.clientHeight) ? (el.scrollHeight)+"px" : "170px";
+    }
+
+    // let tweetMedias = <div id='tweet-medias'>{imageIcon()} {gifIcon()} {pollIcon()} {copyIcon()} {scheduleIcon()}</div>
+    let tweetMedias = <div id='tweet-medias'><div id='image-icon'>{imageIcon()}</div> <div id='gif-icon'>{gifIcon()}</div> <div id='poll-icon'>{pollIcon()}</div> <div id='emoji-icon'>{copyIcon()}</div> <div id='schedule-icon'>{scheduleIcon()}</div></div>
 
     return (
         <div id='tweet-modal' style={{display: toggleModality ? 'block' : 'none'}} >
@@ -68,11 +86,19 @@ function TweetModal() {
                 <div id='header-section'>
                     <img id='profile-pic' src='https://picsum.photos/200/300' />
                     {/* <input id='tweet-input' type='text' value={tweetText} onChange={handleTweetTextChanges} placeholder="What's happening?" /> */}
+                    {/* <textarea contentEditable rows='4' id='tweet-input' type='text' value={tweetText} onChange={handleTweetTextChanges} placeholder="What's happening?" /> */}
+                    {/* <textarea onKeyUp={adjustHeight} rows='4' id='tweet-input' type='text' value={tweetText} onChange={handleTweetTextChanges} placeholder="What's happening?" /> */}
                     <textarea rows='4' id='tweet-input' type='text' value={tweetText} onChange={handleTweetTextChanges} placeholder="What's happening?" />
                 </div>
                 <div id='footer-section'>
                     {/* <span>{everybodyIcon()}</span> <span>everybody can reply</span> */}
                     <span id='options-selected'>{tweetPrivacy == '01' ? tweetPrivacySelected01() : tweetPrivacy == '02' ? tweetPrivacySelected02() : tweetPrivacySelected03()}</span>
+                    <div id='tweet-additionals'>
+                        {/* <div id='tweet-medias'></div> */}
+                        {tweetMedias}
+                        <div id='tweet-now'>+ tweet</div>
+                    </div>
+                    <GridDemo />
                     <div id='tweet-options' style={{display: tweetOptions ? 'block' : 'none'}}>
                         <h4>Who can reply?</h4>
                         <p>Choose who can reply to this Tweet. Anyone mentioned can always reply.</p>
@@ -100,5 +126,12 @@ let tweetPrivacySelected01 = () => <span className='privacy-spans'><span classNa
 let tweetPrivacySelected02 = () => <span className='privacy-spans'><span className='privacy-svg'>{peopleIcon('rgb(29, 155, 240)')}</span> <span className='privacy-text'>People you follow</span></span>
 
 let tweetPrivacySelected03 = () => <span className='privacy-spans'><span className='privacy-svg'>{mentionedIcon('rgb(29, 155, 240)')}</span> <span className='privacy-text'>Only people you mention</span></span>
+
+let GridDemo = ({onGifClick}) => {
+    const fetchGifs = (offset) =>
+    giphyFetch.trending({ offset, limit: 10 });
+    const [width, setWidth] = useState(window.innerWidth);
+    return <Grid fetchGifs={fetchGifs} width={width} columns={4} gutter={6} />
+}
 
 export default TweetModal
